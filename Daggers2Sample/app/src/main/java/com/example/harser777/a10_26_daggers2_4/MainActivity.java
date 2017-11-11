0146ca8771c7f294e1706762a2c76b740b8e437f
Harold Serna <harsc8@gmail.com>
2017-11-11 15:58:45 -0500

    clean code

--- Daggers2Sample/app/src/main/java/com/example/harser777/a10_26_daggers2_4/MainActivity.java
+++ Daggers2Sample/app/src/main/java/com/example/harser777/a10_26_daggers2_4/MainActivity.java
@@ -3,21 +3,21 @@ package com.example.harser777.a10_26_daggers2_4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import com.example.harser777.a10_26_daggers2_4.Dependencies.Computer;
-
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Inject Computer computer;
+    @Inject 
+    Computer computer;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         computer=((MyApp)getApplication()).getComponent().provideComputer();
-
+         //test
         Toast.makeText(this, computer.printSpecs(), Toast.LENGTH_LONG).show();
     }
 }
